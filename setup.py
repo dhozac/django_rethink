deaa328471775be97501c4ea5c53876a12187822
Daniel Hokka Zakrisson <daniel@hozac.com>
2018-03-22 14:28:41 +0100

    Update URL and such in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,20 @@ import re
 
 setup(name='django_rethink',
       version='0.3.12',
+      license='Apache Software License',
       description='Library to use RethinkDB with Django REST framework',
-      author='Klarna IT Operations Core Services',
-      author_email='itops.core-services@klarna.com',
-      url='',
+      author='Klarna Bank AB',
+      author_email='daniel.zakrisson@klarna.com',
+      url='https://github.com/dhozac/django_rethink',
       packages=['django_rethink', 'django_rethink.management', 'django_rethink.management.commands'],
       install_requires=map(lambda x: re.sub(r".*#egg=(.*)", lambda m: m.group(1), x.strip()), open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()),
       include_package_data=True,
       zip_safe=True,
+      classifiers=[
+          'Development Status :: 6 - Mature',
+          'Environment :: Web Environment',
+          'Framework :: Django :: 1.11',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+      ],
 )
