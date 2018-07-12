de4a581d77b0c8f5f66b6a0f1cfa5faf342c0284
Daniel Hokka Zakrisson <daniel@hozac.com>
2018-07-12 10:45:43 +0200

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import re
 
 setup(name='django_rethink',
-      version='0.3.18',
+      version='0.4.0',
       license='Apache Software License',
       description='Library to use RethinkDB with Django REST framework',
       author='Klarna Bank AB',
